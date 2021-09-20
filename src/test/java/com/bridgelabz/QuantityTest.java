4381c679441e4c31ade4f487bb1fd3c66c7fcd52
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-09-20 13:49:05 +0530

    ADDED : Code and Test for Feet equality check

--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -11,4 +11,33 @@ public class QuantityTest {
         Feet feet2 = new Feet(0.0);
         Assertions.assertEquals(feet1, feet2);
     }
+
+    @Test
+    void given0FeetAnd1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assertions.assertNotEquals(feet1, feet2);
+    }
+
+    @Test
+    void given0FeetAndNull_ShouldReturnNotEquals() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = null;
+        Assertions.assertNotEquals(feet1, feet2);
+    }
+
+    @Test
+    void givenFeetAndFeetFromSameRef_WhenEqual_ShouldReturnTrue() {
+        Feet feet1 = new Feet(0.0);
+        boolean actual = feet1 == feet1;    // '==' operator is for ref obj
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void givenFeetAndFeetFromDiffRef_WhenNotEqual_ShouldReturnFalse() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        boolean actual = feet1 == feet2;   // '==' operator is for ref obj
+        Assertions.assertFalse(actual);
+    }
 }
