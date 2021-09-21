be8803bb758afe754512271acd110d754cb73bfa
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-09-21 19:17:22 +0530

    ADDED : CM Unit, Tested Equality and Comparison for CM and Inch

--- src/main/java/com/quantityMeasurement/Length.java
+++ src/main/java/com/quantityMeasurement/Length.java
@@ -1,8 +1,9 @@
 package com.quantityMeasurement;
 
 public class Length {
-    enum Unit {FEET, YARD, INCH}
+    enum Unit {FEET, YARD, CENTI_METER, INCH}
 
+    private static final double INCH_TO_CM = 2.5;
     private static final double FEET_TO_INCH = 12.0;
     private static final double YARD_TO_FEET = 3.0;
     private static final double YARD_TO_INCH = 36.0;
@@ -19,21 +20,20 @@ public class Length {
             return this.equals(that);
         if (this.unit.equals(Unit.FEET) && that.unit.equals(Unit.INCH))
             return Double.compare(this.value * FEET_TO_INCH, that.value) == 0;
-
         if (this.unit.equals(Unit.INCH) && that.unit.equals(Unit.FEET))
             return Double.compare(this.value, that.value * FEET_TO_INCH) == 0;
-
         if (this.unit.equals(Unit.FEET) && that.unit.equals(Unit.YARD))
             return Double.compare(this.value, that.value * YARD_TO_FEET) == 0;
-
-        if (this.unit.equals(Unit.INCH) && that.unit.equals(Unit.YARD))
-            return Double.compare(this.value, that.value * YARD_TO_INCH) == 0;
-
-        if (this.unit.equals(Unit.YARD) && that.unit.equals(Unit.INCH))
-            return Double.compare(this.value * YARD_TO_INCH, that.value) == 0;
-
         if (this.unit.equals(Unit.YARD) && that.unit.equals(Unit.FEET))
             return Double.compare(this.value * YARD_TO_FEET, that.value) == 0;
+        if (this.unit.equals(Unit.INCH) && that.unit.equals(Unit.YARD))
+            return Double.compare(this.value, that.value * YARD_TO_INCH) == 0;
+        if (this.unit.equals(Unit.YARD) && that.unit.equals(Unit.INCH))
+            return Double.compare(this.value * YARD_TO_INCH, that.value) == 0;
+        if (this.unit.equals(Unit.INCH) && that.unit.equals(Unit.CENTI_METER))
+            return Double.compare(this.value * INCH_TO_CM, that.value) == 0;
+        if (this.unit.equals(Unit.CENTI_METER) && that.unit.equals(Unit.INCH))
+            return Double.compare(this.value, that.value * INCH_TO_CM) == 0;
         return false;
     }
 
